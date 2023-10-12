0ae07285be63e354acab7093abf8cfaadf5663ad
ru55-2 <rm_leavitt@icloud.com>
2023-10-12 12:24:01 -0600

    my first website

---
+++ script.js
@@ -0,0 +1,118 @@
+window.onload = car1;
+
+function car1(){
+    const carimage = document.getElementById('image');
+
+    
+    document.getElementById("history").innerHTML = "The McLaren F1 GTR is an iconic racing car that emerged from the acclaimed McLaren F1 road car, conceived by the brilliant minds of Gordon Murray and Ron Dennis. The F1 GTR made its debut in the 1995 24 Hours of Le Mans, marking McLaren's return to top-tier motorsport after a long hiatus. With its striking aerodynamics, potent BMW V12 engine, and meticulous engineering, the F1 GTR quickly gained a reputation as a formidable contender on the racing circuit. In its debut year at Le Mans, it secured an astonishing victory, cementing its place in racing history. Over the next few years, the F1 GTR continued to dominate various endurance races, including the BPR Global GT Series. Its success not only solidified McLaren's presence in motorsport but also elevated the F1 GTR to legendary status, making it a cherished icon among racing enthusiasts and collectors alike.";
+
+    document.getElementById("engine").innerHTML = "<li>Type: BMW S70/2 6.1-liter naturally aspirated V12</li><li>Power Output: Approximately 600 horsepower</li><li>Torque: Approximately 480 lb-ft (651 Nm)</li>";
+    
+    document.getElementById("performance").innerHTML = "<li>0 to 60 mph (0 to 97 km/h): Approximately 3.2 seconds</li><li>Top Speed: Approximately 220 mph (354 km/h)</li>";
+    
+    document.getElementById("transmission").innerHTML = "<li>Six-speed manual gearbox</li>";
+    
+    document.getElementById("weight").innerHTML = "<li>Curb Weight: Approximately 2,315 pounds (1,050 kg)</li>";
+    
+    document.getElementById("chassis and body").innerHTML = "<li>Chassis: Carbon fiber monocoque</li><li>Body: Aerodynamically enhanced bodywork for racing</li>";
+    
+    document.getElementById("suspension").innerHTML = "<li>Front Suspension: Double wishbone with coil springs</li><li>Rear Suspension: Double wishbone with coil springs</li>";
+    
+    document.getElementById("brakes").innerHTML = "<li>Front Brakes: Ventilated disc brakes</li><li>Rear Brakes: Ventilated disc brakes</li>";
+    
+    document.getElementById("wheels and tires").innerHTML = "<li>Wheels: Center-lock racing wheels</li><li>Tires: Specially designed racing tires</li>";
+    
+    document.getElementById("dimensions").innerHTML = "<li>Length: Approximately 4,923 mm (193.8 inches)</li><li>Width: Approximately 1,820 mm (71.7 inches)</li><li>Height: Approximately 1,140 mm (44.9 inches)</li><li>Wheelbase: Approximately 2,718 mm (107.0 inches)</li>";
+    
+    document.getElementById("interior").innerHTML = "<li>Seating: Driver's seat with central driving position and two passenger seats</li><li>Steering Wheel: Three-spoke racing steering wheel</li><li>Dashboard: Minimalistic design with essential controls</li><li>Materials: High-quality materials, including leather and carbon fiber</li>";
+    
+    carimage.src = './sources/car1.jpg';
+    
+
+
+}
+
+function car2(){
+    const carimage = document.getElementById('image');
+
+    document.getElementById("history").innerHTML = "The Mercedes-Benz CLK-GTR is a legendary racing car that emerged in response to the FIA GT1 Championship in 1997. This striking machine was derived from the CLK road car but underwent extensive modifications to become a purpose-built race car. With its potent 6.9-liter V12 engine producing over 600 horsepower and paired with a six-speed sequential gearbox, the CLK-GTR dominated the racing circuit. In its debut season, it clinched the FIA GT1 Constructors' Championship, solidifying its place in motorsport history. The car's distinctive design, featuring aggressive aerodynamics and a wide stance, made it an iconic sight on the racetrack. Today, the Mercedes-Benz CLK-GTR remains a rare and revered gem among collectors and enthusiasts.";
+    
+    document.getElementById("engine").innerHTML = "<li>Type: Mercedes-Benz M120 6.9-liter naturally aspirated V12</li><li>Power Output: Approximately 604 horsepower</li><li>Torque: Approximately 572 lb-ft (777 Nm)</li>";
+    
+    document.getElementById("performance").innerHTML = "<li>0 to 60 mph (0 to 97 km/h): Approximately 3.8 seconds</li><li>Top Speed: Approximately 214 mph (345 km/h)</li>";
+    
+    document.getElementById("transmission").innerHTML = "<li>Six-speed sequential gearbox</li>";
+    
+    document.getElementById("weight").innerHTML = "<li>Curb Weight: Approximately 2,976 pounds (1,350 kg)</li>";
+    
+    document.getElementById("chassis and body").innerHTML = "<li>Chassis: Carbon fiber monocoque</li><li>Body: Aggressively styled, race-inspired bodywork</li>";
+    
+    document.getElementById("suspension").innerHTML = "<li>Front Suspension: Independent double wishbone</li><li>Rear Suspension: Independent double wishbone</li>";
+    
+    document.getElementById("brakes").innerHTML = "<li>Front Brakes: Ventilated disc brakes</li><li>Rear Brakes: Ventilated disc brakes</li>";
+    
+    document.getElementById("wheels and tires").innerHTML = "<li>Wheels: Lightweight racing wheels</li><li>Tires: Specially designed racing tires</li>";
+    
+    document.getElementById("dimensions").innerHTML = "<li>Length: Approximately 4,600 mm (181.1 inches)</li><li>Width: Approximately 1,985 mm (78.1 inches)</li><li>Height: Approximately 1,220 mm (48.0 inches)</li><li>Wheelbase: Approximately 2,770 mm (109.1 inches)</li>";
+    
+    document.getElementById("interior").innerHTML = "<li>Seating: Driver's seat with central driving position and one passenger seat</li><li>Steering Wheel: Racing steering wheel with essential controls</li><li>Materials: Lightweight materials, carbon fiber, and Alcantara upholstery</li>";
+    
+    carimage.src = './sources/car2.jpg'; 
+}
+
+function car3(){
+    const carimage = document.getElementById('image');
+
+    document.getElementById("history").innerHTML = "The Porsche 911 GT1 is a legendary racing car born from the prestigious Porsche 911 lineage. This remarkable machine was developed to compete in the GT1 class, and it made its debut in 1996. With its distinctively sleek and aerodynamic design, the 911 GT1 was a standout on the racetrack. It was powered by a potent 3.2-liter twin-turbocharged flat-six engine, producing over 600 horsepower. Paired with a six-speed manual gearbox, the 911 GT1 showcased remarkable performance. In its inaugural season, it secured an impressive victory at the 24 Hours of Le Mans, cementing its place in motorsport history. Over the years, it continued to excel in various endurance races, making it an icon among racing enthusiasts and collectors alike.";
+    
+    document.getElementById("engine").innerHTML = "<li>Type: Porsche 3.2-liter twin-turbocharged flat-six</li><li>Power Output: Approximately 600 horsepower</li><li>Torque: Approximately 480 lb-ft (651 Nm)</li>";
+    
+    document.getElementById("performance").innerHTML = "<li>0 to 60 mph (0 to 97 km/h): Approximately 3.7 seconds</li><li>Top Speed: Approximately 205 mph (330 km/h)</li>";
+    
+    document.getElementById("transmission").innerHTML = "<li>Six-speed manual gearbox</li>";
+    
+    document.getElementById("weight").innerHTML = "<li>Curb Weight: Approximately 2,315 pounds (1,050 kg)</li>";
+    
+    document.getElementById("chassis and body").innerHTML = "<li>Chassis: Lightweight carbon fiber monocoque</li><li>Body: Sleek and aerodynamic design</li>";
+    
+    document.getElementById("suspension").innerHTML = "<li>Front Suspension: Double wishbone</li><li>Rear Suspension: Double wishbone</li>";
+    
+    document.getElementById("brakes").innerHTML = "<li>Front Brakes: Ventilated disc brakes</li><li>Rear Brakes: Ventilated disc brakes</li>";
+    
+    document.getElementById("wheels and tires").innerHTML = "<li>Wheels: Lightweight racing wheels</li><li>Tires: Specially designed racing tires</li>";
+    
+    document.getElementById("dimensions").innerHTML = "<li>Length: Approximately 4,500 mm (177.2 inches)</li><li>Width: Approximately 1,990 mm (78.3 inches)</li><li>Height: Approximately 1,100 mm (43.3 inches)</li><li>Wheelbase: Approximately 2,880 mm (113.4 inches)</li>";
+    
+    document.getElementById("interior").innerHTML = "<li>Seating: Driver's seat with central driving position and one passenger seat</li><li>Steering Wheel: Racing steering wheel with essential controls</li><li>Dashboard: Minimalistic design with essential controls</li><li>Materials: Lightweight materials, including carbon fiber</li>";
+    
+    carimage.src = './sources/car3.jpg'; 
+    
+}
+
+function car4(){
+    const carimage = document.getElementById('image');
+
+    document.getElementById("history").innerHTML = "The Nissan R390 GT1 is a remarkable racing car that made its debut in the 1997 FIA GT1 Championship. Born out of Nissan's ambition to compete at the highest level of motorsport, the R390 GT1 was a formidable contender. Its sleek and aerodynamic design, powered by a twin-turbocharged 3.5-liter V8 engine producing around 550 horsepower, allowed it to reach impressive speeds on the track. The car's racing career was highlighted by its remarkable performance in the 24 Hours of Le Mans, where it achieved notable results. The Nissan R390 GT1's combination of power, innovation, and striking design cemented its status as a celebrated icon among racing enthusiasts and fans."
+    
+    document.getElementById("engine").innerHTML = "<li>Type: Twin-turbocharged 3.5-liter V8</li><li>Power Output: Approximately 550 horsepower</li><li>Torque: Approximately 470 lb-ft (637 Nm)</li>";
+    
+    document.getElementById("performance").innerHTML = "<li>0 to 60 mph (0 to 97 km/h): Approximately 3.9 seconds</li><li>Top Speed: Approximately 220 mph (354 km/h)</li>";
+    
+    document.getElementById("transmission").innerHTML = "<li>Six-speed manual gearbox</li>";
+    
+    document.getElementById("weight").innerHTML = "<li>Curb Weight: Approximately 2,645 pounds (1,200 kg)</li>";
+    
+    document.getElementById("chassis and body").innerHTML = "<li>Chassis: Carbon fiber monocoque</li><li>Body: Streamlined aerodynamic bodywork for racing</li>";
+    
+    document.getElementById("suspension").innerHTML = "<li>Front Suspension: Independent double wishbone</li><li>Rear Suspension: Independent double wishbone</li>";
+    
+    document.getElementById("brakes").innerHTML = "<li>Front Brakes: Ventilated disc brakes</li><li>Rear Brakes: Ventilated disc brakes</li>";
+    
+    document.getElementById("wheels and tires").innerHTML = "<li>Wheels: Lightweight racing wheels</li><li>Tires: Specially designed racing tires</li>";
+    
+    document.getElementById("dimensions").innerHTML = "<li>Length: Approximately 4,670 mm (183.9 inches)</li><li>Width: Approximately 1,950 mm (76.8 inches)</li><li>Height: Approximately 1,150 mm (45.3 inches)</li><li>Wheelbase: Approximately 2,750 mm (108.3 inches)</li>";
+    
+    document.getElementById("interior").innerHTML = "<li>Seating: Driver's seat with central driving position and one passenger seat</li><li>Steering Wheel: Racing steering wheel with essential controls</li><li>Dashboard: Minimalistic design with essential racing instrumentation</li><li>Materials: Lightweight materials, including carbon fiber and racing-grade upholstery</li>";
+    
+    carimage.src = './sources/car4.jpg'; 
+}
